e1a21a823186254a404aae934da7b3e113f70292
ask2696 <ask_2696@gmail.com>
2017-07-17 01:10:59 +0530

    Added comments

--- BBB/can_py/main_sockets.py
+++ BBB/can_py/main_sockets.py
@@ -2,13 +2,14 @@
  
 import socket   #for sockets
 import sys  #for exit
+from 
  
 #create an INET, STREAMing socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
     print 'Failed to create socket'
-    sys.exit()
+    #sys.exit()
      
 print 'Socket Created'
  
