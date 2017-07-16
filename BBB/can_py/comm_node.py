46cc40409429b0fa8d63518f6f417031ed4cae1c
ask2696 <ask_2696@gmail.com>
2017-07-17 01:10:04 +0530

    Started working on receiving commands from socket

--- BBB/can_py/comm_node.py
+++ BBB/can_py/comm_node.py
@@ -11,16 +11,23 @@ can.rc['interface'] = 'socketcan_ctypes'
 from can.interfaces.interface import Bus
 from can import Message
 
-send_messages = {"Start": "00000000","Stop": "00000000", "Emergency Stop":"00000000",
+
+# Dict to set the bits for the requisite command
+send_messages_can = {"Start": "00000000","Stop": "00000000", "Emergency Stop":"00000000",
                     "Engage Brakes":"00000000", "Disengage Brakes":"00000000",
                     "Engage Clutch":"00000000", "Disengage Clutch":"00000000",
                     "Actuate Levitation Module":"00000000","Unactuate Levitation Module":"00000000",
                      "Engage Auxillary Propulsion":"00000000","Disengage Auxillary Propulsion":"00000000" }
 
+rcv_messgaes_socket = ["Start","Stop","Emergency Stop","Engage Brakes","Disengage Brakes","Engage Clutch", 
+                    "Disengage Clutch","Actuate Levitation Module","Unactuate Levitation Module",
+                    "Engage Auxillary Propulsion","Disengage Auxillary Propulsion"]
+
+
 
 data_rcv = None;
 
-remote_ip = "192.168.0.51"
+remote_ip = "127.0.0.1"#"192.168.0.51"
 host = 'Base Station';
 port = 3000;
 
@@ -60,24 +67,27 @@ def main():
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((remote_ip , port))
     except socket.error:
         print 'Failed to create socket'
-        sys.exit()
+        #sys.exit()
 
-    s.connect((remote_ip , port))
+    
     message = "Ping from pod!! \n"
     no_recv_packets = 1;
-    
+    #print "#"
     send_flag = False
     data =bytearray([1, 2, 3]);
     print send_messages
     
     try:
         while True:
-            
+            #print "Sane"
             reply = s.recv(4096)
             print "Socket Data"
             print reply
+
+
 
             Message_rcv = bus.recv(0.0)
             if Message_rcv:
@@ -90,6 +100,9 @@ def main():
 
     except  KeyboardInterrupt:
         bus.shutdown()
+
+    except socket.error:
+        print "Socket Send/Receive Error"
 
 
 
