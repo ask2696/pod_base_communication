9d12089503913d5c49d8f32cbfc326e056cbb241
ask2696 <ask_2696@gmail.com>
2017-07-18 00:31:14 +0530

    Added comments

--- BBB/can_py/comm_node.py
+++ BBB/can_py/comm_node.py
@@ -13,36 +13,48 @@ from can.interfaces.interface import Bus
 from can import Message
 
 
-# Dict to set the bits for the requisite command
+# Dict to set the bits for the respective command
+#Need to test it with Teensy Network and update the data formats
 send_messages_can = {"Start": "00000000","Stop": "00000000", "Emergency Stop":"00000000",
                     "Engage Brakes":"00000000", "Disengage Brakes":"00000000",
                     "Engage Clutch":"00000000", "Disengage Clutch":"00000000",
                     "Actuate Levitation Module":"00000000","Unactuate Levitation Module":"00000000",
                      "Engage Auxillary Propulsion":"00000000","Disengage Auxillary Propulsion":"00000000" }
 
+#List of commands that can be sent by the base station
 rcv_messgaes_socket = ["Start","Stop","Emergency Stop","Engage Brakes","Disengage Brakes","Engage Clutch", 
                     "Disengage Clutch","Actuate Levitation Module","Unactuate Levitation Module",
                     "Engage Auxillary Propulsion","Disengage Auxillary Propulsion"]
 
+#Dict to update the data received from the can network
+#Created to have different freqs of transmission between (comm node,base) and (comm node, can network)
+#Log this if needed
 pod_data_packet_can = {"team_id":0,"stat":0,"acceleration":0,"position":0,"velocity":0,"battery_voltage":0,
                     "battery_current":0,"battery_temperature":0,"pod_temperature":0,"stripe_count":0}
 
+
+#Dict to update the data that needs to be send to the Base Station backend
+#Created to have different freqs of transmission between (comm node,base) and (comm node, can network)
 pod_data_packet_socket = {"team_id":0,"stat":0,"acceleration":0,"position":0,"velocity":0,"battery_voltage":0,
                     "battery_current":0,"battery_temperature":0,"pod_temperature":0,"stripe_count":0}
 
+#data frame id info for different nodes
 nodes_data_frames_id = {"power_node":"","nav_node":"","control_node":""}
-
+#emergency frame id info for different nodes
 node_emergency_frames_id = {"power_node":"","nav_node":"","control_node":""}
 
 comm_node_header ={}
 
 data_rcv = None;
 
+#Set the ip of the base station when deployed
+#Use the local ip in the network
 remote_ip = "127.0.0.1"#"192.168.0.51"
 host = 'Base Station';
-port = 3000;
+port = 3000;#Port for TCP comm
 
 def can_rcv(Message):
+    #Recevie the can packet and decode the node origin and call update functions
     now = datetime.datetime.now()
     timeString = now.strftime("%d.%m.%Y %H:%M:%S")
 
@@ -58,7 +70,7 @@ def can_rcv(Message):
     return Message.data[0:8]
 
 def can_send():
-    
+    #Set the can packet to send the data on the canbus
     Message.extended_id = False
     Message.is_remote_frame = False
     Message.id_type = False
@@ -92,6 +104,7 @@ def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((remote_ip , port))
+        #print "debug 1",conn
     except socket.error:
         print 'Failed to create socket'
         #sys.exit()
@@ -106,7 +119,7 @@ def main():
     no_packets_sent =0;    
     try:
         while True:
-            #print "Sane"
+            #print "#"
             #json_pod_data = json.dumps(pod_data_packet_socket)
             #json_pod_data = json.loads(json_pod_data)
 
@@ -114,6 +127,7 @@ def main():
             no_packets_sent = no_packets_sent+1;
             reply = s.recv(4096)
             #print "Socket Data"
+            #print type(reply)
             if(reply != "NULL"):
                 print reply
 
