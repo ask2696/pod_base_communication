fdba52198ffbe3c564fffec6f9364c6fe9f4b8b2
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2017-07-19 21:29:56 +0530

    Make Left sidebar sticky

---
+++ Base/ui/public/js/layout.js
@@ -0,0 +1,23 @@
+$('#left-sidebar-control').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: false, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    }
+  );
+$('#right-sidebar-control').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: false, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    }
+  );
+
+// Initialize modal
+$(document).ready(function(){
+    $('.modal').modal();
+});
