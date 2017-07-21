7afb72931779bf7302425c28dbb0d3e8ef4844e6
Poulami Sarkar <poulamirulz@gmail.com>
2017-07-21 15:31:47 +0530

    Changed toggles

--- Base/ui/public/js/slider.js
+++ Base/ui/public/js/slider.js
@@ -1,42 +1,20 @@
-(function() {
-  $("#range").slider({
+for(var i=1; i<=2; i++) {
+  $("#range"+i).slider({
     range: "min",
     max: 100,
     value: 0,
-    slide: function(e, ui) {
-      $("#currentVal").val(ui.value);
+    slide: function(e, ui, i=1) {
+      console.log(i);
+      $("#currentVal"+i).val(ui.value);
     }
   });
-
-}).call(this);
-
-$('#currentVal').change(function() {
-  if(($("#currentVal").val()<=100) &&($("#currentVal").val()>=0) ){
-    $("#range").slider({
-    range: "min",
-    max: 100,
-    value: $("#currentVal").val()
-    });
-  }
-})
-(function() {
-  $("#range1").slider({
-    range: "min",
-    max: 100,
-    value: 0,
-    slide: function(e, ui) {
-      $("#currentVal").html(ui.value);
+  $('#range-value'+i).change(function(i=1) {
+    if(($("#currentVal"+i).val()<=100) &&($("#currentVal"+i).val()>=0) ){
+      $("#range"+i).slider({
+      range: "min",
+      max: 100,
+      value: $("#currentVal"+i).val()
+      });
     }
   });
-
-}).call(this);
-
-$('#currentVal').change(function() {
-  if(($("#currentVal").val()<=100) &&($("#currentVal").val()>=0) ){
-    $("#range1").slider({
-    range: "min",
-    max: 100,
-    value: $("#currentVal").val()
-    });
-  }
-})
+}
