ca2b3003434bf84a9944ff624d1d568253dbaea6
Poulami Sarkar <poulamirulz@gmail.com>
2017-07-21 12:36:55 +0530

    Added input for sliders

--- Base/ui/public/js/slider.js
+++ Base/ui/public/js/slider.js
@@ -4,11 +4,21 @@
     max: 100,
     value: 0,
     slide: function(e, ui) {
-      $("#currentVal").html(ui.value);
+      $("#currentVal").val(ui.value);
     }
   });
 
 }).call(this);
+
+$('#currentVal').change(function() {
+  if(($("#currentVal").val()<=100) &&($("#currentVal").val()>=0) ){
+    $("#range").slider({
+    range: "min",
+    max: 100,
+    value: $("#currentVal").val()
+    });
+  }
+})
 (function() {
   $("#range1").slider({
     range: "min",
@@ -20,3 +30,13 @@
   });
 
 }).call(this);
+
+$('#currentVal').change(function() {
+  if(($("#currentVal").val()<=100) &&($("#currentVal").val()>=0) ){
+    $("#range1").slider({
+    range: "min",
+    max: 100,
+    value: $("#currentVal").val()
+    });
+  }
+})
