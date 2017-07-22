7d76caf6658e8c051d42e0e25791fcabd0969fe5
Poulami Sarkar <poulamirulz@gmail.com>
2017-07-22 15:59:37 +0530

    small changes in update.js

--- Base/ui/public/js/update.js
+++ Base/ui/public/js/update.js
@@ -1,7 +1,7 @@
 function update(data){
     $('.temp').text(data.pod_temperature);
     $('#team_id').text(data.team_id);
-    $('.pressure').text(pod)
+    $('.pressure').text(pod_pressure);
     $('.currentvalue').text(data.battery_current);
     $('.tempvalue').text(data.battery_temperature);
     p.updateStripCount(data.stripe_count);
@@ -12,22 +12,22 @@ function update(data){
         value2 = Math.round(Math.random() * 360),
         value3 = Math.round(Math.random() * 360);
 
-    yaw.setValueAnimated(yaw-value,0.5);
-    pitch.setValueAnimated(pitch-value, 0.5);
-    roll.setValueAnimated(roll-value, 0.5);
+    yaw.setValueAnimated(yaw_value,0.5);
+    pitch.setValueAnimated(pitch_value, 0.5);
+    roll.setValueAnimated(roll_value, 0.5);
     for (var i ; i<= 4;i++ )
     {
-      $('#lev'+i).text(6);//Don't know how to update the values
+      $('#lev'+i).text(levitation_value);//Don't know how to update the values
     }
-    $('#brake-pad').text(break-pad-distance);
+    $('#brake-pad').text(break_pad_distance);
     for(var i=0; i<=4;i++){
     $('#pod-status').text(status);
     if(pusher_value==0){
-      $(.pusher).removeClass('engage');
-      $(.pusher).addClass('disengage');
+      $('.pusher').removeClass('engage');
+      $('.pusher').addClass('disengage');
     }
     else {
-      $(.pusher).removeClass('disengage');
-      $(.pusher).addClass('engage');
+      $('.pusher').removeClass('disengage');
+      $('.pusher').addClass('engage');
     }
 }
