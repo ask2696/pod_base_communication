71f0c1800cd469e24e65007f0a64753bb7d4d1fc
Poulami Sarkar <poulamirulz@gmail.com>
2017-07-22 15:22:44 +0530

    velocity/acceleration update

--- Base/ui/public/js/update.js
+++ Base/ui/public/js/update.js
@@ -1,19 +1,26 @@
 function update(data){
     $('.temp').text(data.pod_temperature);
     $('#team_id').text(data.team_id);
-    //$('.pressure').text(pod)
+    $('.pressure').text(pod)
     $('.currentvalue').text(data.battery_current);
     $('.tempvalue').text(data.battery_temperature);
     p.updateStripCount(data.stripe_count);
+    v.setStatValue(data.velocity);
+    a.setStatValue(data.acceleration);
     // setValueAnimated(value, durationInSecs);
     var value1 = Math.round(Math.random() * 360),
-      value2 = Math.round(Math.random() * 360),
-      value3 = Math.round(Math.random() * 360);
+        value2 = Math.round(Math.random() * 360),
+        value3 = Math.round(Math.random() * 360);
+
     yaw.setValueAnimated(value1, 1);
     pitch.setValueAnimated(value2, 2);
     roll.setValueAnimated(value3, 1.5);
     for (var i ; i<= 4;i++ )
     {
-     $('#lev'+i).text(6);//Don't know how to update the values
+      $('#lev'+i).text(6);//Don't know how to update the values
+    }
+    $('#brake-pad').text(break-pad-distance);
+    for(var i=0; i<=4;i++){
+      $('')
     }
 }
